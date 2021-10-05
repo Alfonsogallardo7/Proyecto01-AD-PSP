cc763312e729c3a0a13bef1482ac98886fcd293a
Manuel Spinola Tubio <spinola.tuman21@triana.salesianos.edu>
2021-10-06 00:01:37 +0200

    Añadido método en PlayListDtoConverter, casi terminado el PlayListConverter, falta averiguar como crear Playlist con lista de canciones

---
+++ Proyecto01/src/main/java/com/salesianostriana/Proyecto01/controller/PlayListController.java
@@ -0,0 +1,83 @@
+package com.salesianostriana.Proyecto01.controller;
+
+import com.salesianostriana.Proyecto01.dto.CreateCancionesDto;
+import com.salesianostriana.Proyecto01.dto.CreatePlaylistDto;
+import com.salesianostriana.Proyecto01.dto.GetPlaylistDto;
+import com.salesianostriana.Proyecto01.dto.PlaylistDtoConverter;
+import com.salesianostriana.Proyecto01.model.Canciones;
+import com.salesianostriana.Proyecto01.model.Playlist;
+import com.salesianostriana.Proyecto01.repository.CancionesRepository;
+import com.salesianostriana.Proyecto01.repository.PlaylistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/playlist")
+public class PlayListController {
+
+    private final PlaylistRepository playlistRepository;
+    private final PlaylistDtoConverter dtoConverter;
+    private final CancionesRepository cancionesRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Playlist>> findAll(){
+        List<Playlist> data = playlistRepository.findAll();
+        if (data.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }else {
+
+            return ResponseEntity.
+                    ok()
+                    .body(playlistRepository.findAll());
+        }
+
+    }
+
+    @PostMapping("/")
+    public ResponseEntity <Playlist> create(@RequestBody CreatePlaylistDto dto) {
+
+        if (dto.getCancionesId() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Playlist nuevo = dtoConverter.createPlaylistDtoToPlaylist(dto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(playlistRepository.save(nuevo));
+
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Playlist> findOne(@PathVariable Long id){
+
+            return ResponseEntity.of(playlistRepository.findById(id));
+    }
+
+    public ResponseEntity<Playlist> edit(@RequestBody Playlist playlist, @PathVariable Long id){
+
+
+        return ResponseEntity.of(
+                playlistRepository.findById(id).map(a->{
+                    a.setDescripcion(playlist.getDescripcion());
+                    a.setNombre(playlist.getNombre());
+                    a.setCanciones(playlist.getCanciones());
+                    playlistRepository.save(a);
+                    return a;
+                })
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        playlistRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
