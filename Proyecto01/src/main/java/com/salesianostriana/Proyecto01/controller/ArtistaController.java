dee14db22433d524592580c749fc34f6d042f557
Daniel De Luna Rodríguez <deluna.rodan21@triana.salesianos.edu>
2021-10-05 12:49:03 +0200

    Añado el controller de artista.HU-02.HU-04.HU-05.

---
+++ Proyecto01/src/main/java/com/salesianostriana/Proyecto01/controller/ArtistaController.java
@@ -0,0 +1,54 @@
+package com.salesianostriana.Proyecto01.controller;
+
+
+import com.salesianostriana.Proyecto01.model.Artista;
+import com.salesianostriana.Proyecto01.repository.ArtistaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/artist")
+@RequiredArgsConstructor
+public class ArtistaController {
+
+    private final ArtistaRepository repository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Artista>> findAll(){
+        return ResponseEntity
+                .ok()
+                .body(repository.findAll());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artista> findOne(@PathVariable Long id){
+        return ResponseEntity
+                .of(repository.findById(id));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Artista> create(@RequestBody Artista nuevoArtista){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(repository.save(nuevoArtista));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Artista> edit(@RequestBody Artista art,@PathVariable Long id){
+        return ResponseEntity.of(
+                repository.findById(id).map(a ->{
+                    a.setNombre(art.getNombre());
+                    repository.save(a);
+                    return a;
+                })
+        );
+    }
+
+
+}
